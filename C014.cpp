0467155418f76f1a1e2a8b9d5dee670ee11744dd
minami20230128 <south614fruit@yahoo.co.jp>
2023-08-21 19:00:00 +0900

    修正

--- C014.cpp
+++ C014.cpp
@@ -1,17 +1,20 @@
-#include <iosteram>
+#include <iostream>
 
 int main()
 {
     int N, r;
     int ball_number = 1;
-    for(int i = 0; i < N i++)
+    std::cin >> N;
+    std::cin >> r;
+    
+    for(int i = 0; i < N; i++)
     {
         int height, width, depth;
         std::cin >> height;
         std::cin >> width;
         std::cin >> depth;
 
-        if(r * 2 => height && r * 2 => width && r * 2 => depth)
+        if(r * 2 <= height && r * 2 <= width && r * 2 <= depth)
         {
             std::cout << ball_number << std::endl;
         }
