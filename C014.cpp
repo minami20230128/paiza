bd4aeb47fe241f2c2ed54e3ae9982c8ff9898ef9
minami20230128 <south614fruit@yahoo.co.jp>
2023-08-21 19:00:00 +0900

    追加

---
+++ C014.cpp
@@ -0,0 +1,22 @@
+#include <iosteram>
+
+int main()
+{
+    int N, r;
+    int ball_number = 1;
+    for(int i = 0; i < N i++)
+    {
+        int height, width, depth;
+        std::cin >> height;
+        std::cin >> width;
+        std::cin >> depth;
+
+        if(r * 2 => height && r * 2 => width && r * 2 => depth)
+        {
+            std::cout << ball_number << std::endl;
+        }
+        ball_number++;
+    }
+
+    return 0;
+}
