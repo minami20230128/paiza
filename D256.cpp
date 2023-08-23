2d8efebf0fc4919f4e2133c3d57d0a3dc89bbd8a
minami20230128 <south614fruit@yahoo.co.jp>
2023-08-23 23:20:23 +0900

    追加

---
+++ D256.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+    int A, B;
+    std::cin >> A;
+    std::cin >> B;
+
+    std::cout << A - 100 << std::endl;
+    std::cout << B + 100 << std::endl;
+
+    return 0;
+}
