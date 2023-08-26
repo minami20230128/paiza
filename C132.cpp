a8ae2dd59b4709db071c0222e6965eaac4a117b4
minami20230128 <south614fruit@yahoo.co.jp>
2023-08-27 02:02:25 +0900

    修正

--- C132.cpp
+++ C132.cpp
@@ -1,35 +1,46 @@
 #include <iostream>
-#include <vector>
+#include <map>
+
+bool IsAllRoomsCleaned(std::map<int, bool> IsCleaned)
+{
+    auto itr = IsCleaned.begin();
+    while(itr != IsCleaned.end())
+    {
+        if(itr->second == 0) return false;
+        //std::cout << itr->first << std::endl;
+        //std::cout << itr->second << std::endl;
+        itr++;
+    }
+    return true;
+}
 
 int main()
 {
-    try
+    int N, M;
+    std::cin >> N;
+    std::cin >> M;
+    std::map<int, bool> IsCleaned;
+
+    //vector[idx]は要素が空だったときに例外が発生するのでmapでやる
+    for(int i = 0; i < N; i++)
     {
-        int N, M;
-        std::cin >> N;
-        std::cin >> M;
-        std::vector<bool>IsCleaned(N, false);
-
-        for(int i = 0; i < N; i++)
-        {
-            int L = M * i + 1;
-            //std::cout << L << std::endl;
-            IsCleaned[L % N] = true;
-        }
-
-        if(std::find(IsCleaned.begin(), IsCleaned.end(), false) == IsCleaned.end())
-        {
-            std::cout << "yes" << std::endl;
-        }
-        else
-        {
-            std::cout << "no" << std::endl;
-        }
-
-        return 0;
+        IsCleaned[i] = false;
     }
-    catch(const std::exception& e)
+
+    for(int i = 0; i < N; i++)
     {
-        std::cerr << e.what() << '\n';
+        int L = M * i;
+        IsCleaned[L % N] = true;
     }
+
+    if(IsAllRoomsCleaned(IsCleaned))
+    {
+        std::cout << "yes" << std::endl;
+    }
+    else
+    {
+        std::cout << "no" << std::endl;
+    }
+    
+    return 0;
 }
