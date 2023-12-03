512618e8478f59dda2c4482a9fe63fbc02e4bc99
minami20230128 <south614fruit@yahoo.co.jp>
2023-12-03 21:29:15 +0900

    クラス化

--- C040.cpp
+++ C040.cpp
@@ -2,12 +2,34 @@
 #include <string>
 #include <stdlib.h>
 
+class HeightRange
+{
+    public:
+    float lower_than;
+    float higher_than;
+
+    HeightRange(float lower_than, float higher_than)
+    {
+        this->lower_than = lower_than;
+        this->higher_than = higher_than;
+    }
+
+    void set_higher(float height)
+    {
+        this->higher_than = this->higher_than > height ? this->higher_than : height;
+    }
+
+    void set_lower(float height)
+    {
+        this->lower_than = this->lower_than < height ? this->lower_than : height;
+    }
+};
+
 int main()
 {
     int N;
     std::cin >> N;
-    float lower_than = 200;
-    float higher_than = 0;
+    auto height_range = HeightRange(200, 0);
     for(int i = 0; i < N; i++)
     {
         std::string relation;
@@ -15,19 +37,19 @@ int main()
         float height;
         std::cin >> height;
 
-        if(relation == "le" && lower_than > height)
+        if(relation == "le")
         {
-            lower_than = height;
+            height_range.set_lower(height);
         }
-        else if(relation == "ge" && higher_than < height)
+        else if(relation == "ge" && height_range.higher_than < height)
         {
-            higher_than = height;
+            height_range.set_higher(height);
         }
     }
 
-    printf("%.1lf", higher_than);
+    printf("%.1lf", height_range.higher_than);
     std::cout << " ";
-    printf("%.1lf", lower_than);
+    printf("%.1lf", height_range.lower_than);
     
     return 0;
 }
