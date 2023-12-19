95c1ce603e531ffe14f3a67103a0bfcc5f5e85f9
minami20230128 <south614fruit@yahoo.co.jp>
2023-12-19 22:41:26 +0900

    追加

--- C057.cpp
+++ C057.cpp
@@ -3,7 +3,8 @@
 int main()
 {
     int T, x, y;
-    int farthest = 0;
+    std::cin >> T >> x >> y;
+    int farthest = x;
 
     for(int i = 0; i < T; i++)
     {
@@ -12,10 +13,13 @@ int main()
 
         x += move_x;
         y += move_y;
-
-        if(y < 0) break;
+        
+        //std::cout << x << std::endl;
+        //std::cout << y << std::endl;
         
         farthest = farthest < x ? x : farthest;
+
+        if(y <= 0) break;
     }
 
     std::cout << farthest << std::endl;
