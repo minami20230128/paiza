ce30923f177a8bdce42f9a183f2f555f12a8f93d
minami20230128 <south614fruit@yahoo.co.jp>
2024-05-12 23:05:55 +0900

    クラス化

--- B145.cpp
+++ B145.cpp
@@ -3,85 +3,108 @@
 #include <map>
 #define NOT_FOUND -1
 
-std::pair<int, int> find_coordinates(std::vector<std::vector<int>> bingo, int num, int N)
+class Bingo
 {
-    for(int i = 0; i < N; i++)
+    std::vector<std::vector<int>> sheet;
+    public:
+    Bingo(int sheet_size)
     {
-        for(int j = 0; j < N; j++)
+        this->sheet = std::vector<std::vector<int>>(sheet_size, std::vector<int>(sheet_size));
+        for(int i = 0; i < sheet_size; i++)
         {
-            if(bingo.at(i).at(j) == num)
+            for(int j = 0; j < sheet_size; j++)
             {
-                return std::make_pair(i, j);
+                std::cin >> this->sheet.at(i).at(j);
             }
         }
     }
-    
-    return std::make_pair(NOT_FOUND, NOT_FOUND);
-}
+
+    void hole(int lottery_time)
+    {
+        for(int i = 0; i < lottery_time; i++)
+        {
+            int num;
+            std::cin >> num;
+
+            auto pair = this->find_coordinates(num);
+            //std::cout << pair.first << std::endl;
+            //std::cout << pair.second << std::endl;
+            
+            if(pair.first != NOT_FOUND)
+            {
+                this->sheet.at(pair.first).at(pair.second) = 0;
+            }
+        }
+    }
+
+    int count()
+    {
+        //横のビンゴ数を数える
+        int sheet_size = sheet.size();
+        int bingo_num = 0;
+        for(int i = 0; i < sheet_size; i++)
+        {
+            for(int j = 0; j < sheet_size; j++)
+            {
+                if(this->sheet.at(i).at(j) != 0) break;
+                if(j == sheet_size - 1) bingo_num++;
+            }
+        }
+        
+        //縦のビンゴ数を数える
+        for(int j = 0; j < sheet_size; j++)
+        {
+            for(int i = 0; i < sheet_size; i++)
+            {
+                if(this->sheet.at(i).at(j) != 0) break;
+                if(i == sheet_size - 1) bingo_num++;
+            }
+        }
+        
+        //斜めのビンゴ数を数える
+        for(int i = 0; i < sheet_size; i++)
+        {
+            if(this->sheet.at(i).at(i) != 0) break;
+            if(i == sheet_size - 1) bingo_num++;
+        }
+        
+        for(int i = 0; i < sheet_size; i++)
+        {
+            if(this->sheet.at(i).at((sheet_size - 1) - i) != 0) break;
+            if(i == sheet_size - 1) bingo_num++;
+        }
+
+        return bingo_num;
+    }
+
+    private:
+    std::pair<int, int> find_coordinates(int num)
+    {
+        int sheet_size = this->sheet.size();
+        for(int i = 0; i < sheet_size; i++)
+        {
+            for(int j = 0; j < sheet_size; j++)
+            {
+                if(this->sheet.at(i).at(j) == num)
+                {
+                    return std::make_pair(i, j);
+                }
+            }
+        }
+        
+        return std::make_pair(NOT_FOUND, NOT_FOUND);
+    }
+};
+
+
 
 int main()
 {
-    int N, K;
-    std::cin >> N >> K;
-    std::vector<std::vector<int>> bingo(N, std::vector<int>(N));
-
-    for(int i = 0; i < N; i++)
-    {
-        for(int j = 0; j < N; j++)
-        {
-            std::cin >> bingo.at(i).at(j);
-        }
-    }
-
-    for(int i = 0; i < K; i++)
-    {
-        int num;
-        std::cin >> num;
-
-        auto pair = find_coordinates(bingo, num, N);
-        //std::cout << pair.first << std::endl;
-        //std::cout << pair.second << std::endl;
-        
-        if(pair.first != NOT_FOUND)
-        {
-            bingo.at(pair.first).at(pair.second) = 0;
-        }
-       
-    }
-    
-    //横
-    int bingo_num = 0;
-    for(int i = 0; i < N; i++)
-    {
-        for(int j = 0; j < N; j++)
-        {
-            if(bingo.at(i).at(j) != 0) break;
-            if(j == N - 1) bingo_num++;
-        }
-    }
-    
-    //縦
-    for(int j = 0; j < N; j++)
-    {
-        for(int i = 0; i < N; i++)
-        {
-            if(bingo.at(i).at(j) != 0) break;
-            if(i == N - 1) bingo_num++;
-        }
-    }
-    
-    //斜め
-    for(int i = 0; i < N; i++)
-    {
-        if(bingo.at(i).at(i) != 0) break;
-        if(i == N - 1) bingo_num++;
-    }
-    
-    for(int i = 0; i < N; i++)
-    {
-        if(bingo.at(i).at((N - 1) - i) != 0) break;
-        if(i == N - 1) bingo_num++;
-    }
+    int sheet_size, lottery_time;
+    std::cin >> sheet_size >> lottery_time;
+    auto bingo = Bingo(sheet_size);
+    bingo.hole(lottery_time);
+    int bingo_num = bingo.count();
 
     std::cout << bingo_num;
 
