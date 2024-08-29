e19181cc367fea2823abef7a7552818045319a48
minami20230128 <south614fruit@yahoo.co.jp>
2024-08-29 19:00:00 +0900

    追加

--- C152.cpp
+++ C152.cpp
@@ -1,32 +1,47 @@
 #include <iostream>
-#include <vector>
+#include <chrono>
+#include <string>
+#include <time.h>
 
 int main()
 {
-    int D;
-    std::cin >> D;
-    int prev = -1;
-    std::vector<int> rainbow_days;
-    for(int i = 0; i < D; i++)
-    {
-        int today;
-        std::cin >> today;
+    int N;
+    std::cin >> N;
 
-        if(today == 0 && prev == 2)
-        {
-            rainbow_days.push_back(i);
-        }
-        
-        prev = today;
-    }
-    
-    if(rainbow_days.size() == 0) std::cout << -1;
-    
-    for(int i = 0; i < rainbow_days.size(); i++)
+    int sum_min = 0;
+    int sum_hour = 0;
+
+    for(int i = 0; i < N; i++)
     {
-        std::cout << rainbow_days[i];
-        if(i != rainbow_days.size() - 1) std::cout << " ";
+        std::string start, end;
+        std::cin >> start >> end;
+
+        int start_hour = std::stoi(start.substr(0, 2));
+        int start_min = std::stoi(start.substr(3, 2));
+
+        int end_hour = std::stoi(end.substr(0, 2));
+        int end_min = std::stoi(end.substr(3, 2));
+        
+        //std::cout << start_hour << start_min << std::endl;
+        //std::cout << end_hour << end_min << std::endl;
+        
+
+        int minutes = end_min - start_min > 0 ? end_min - start_min : 60 + (end_min - start_min);
+        int hours = end_min - start_min > 0 ? end_hour - start_hour : end_hour - start_hour - 1;
+
+        sum_min += minutes;
+        sum_hour += hours;
+        
+        if(sum_min >= 60)
+        {
+            sum_hour += sum_min /60;
+            sum_min %= 60;
+        }
     }
+
+    std::cout << sum_hour;
+    std::cout << " ";
+    std::cout << sum_min;
 
     return 0;
 }
